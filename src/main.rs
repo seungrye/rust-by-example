07066361f5514e52b7c6a1901aa967223574412b
Ahn, Seung-rye <seungrye@gmail.com>
2020-03-15 01:31:16 +0900

    list 를 fmt::Display 로 출력하는 예제

--- src/main.rs
+++ src/main.rs
@@ -30,5 +30,25 @@ fn main() {
     functions::closures::main();
     functions::higher_order_functions::main();
 
-    println!("Hello, world!");
+    let v = List(vec![1, 2, 3]);
+
+    println!("{}", v);
+}
+
+struct List(Vec<i32>);
+impl fmt::Display for List {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "[")?;
+
+        let List(ref vec) = *self; // 문법적 의미를 모르겠음;
+
+        for (index, v) in vec.iter().enumerate() {
+            if index != 0 {
+                write!(f, ", ")?;
+            }
+
+            write!(f, "{}: {}", index, v)?;
+        }
+        return write!(f, "]");
+    }
 }
