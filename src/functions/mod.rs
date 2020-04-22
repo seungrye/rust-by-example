6e455fde22eb7020abb7c6cffea1fb5018b89dda
Ahn, Seung-rye <seungrye@gmail.com>
2020-04-22 20:07:05 +0900

    closure 에 대한 예제 추가중.

--- src/functions/mod.rs
+++ src/functions/mod.rs
@@ -5,6 +5,70 @@
 pub fn main() {
     methods();
     closures();
+    capturing();
+    closure_as_input_parameters();
+}
+// 가능한 closure 타입은 다음과 같다.
+// Fn : 클로저의 캡처 변수를 참조로 획득 (&T)
+// FnMut : 클로저의 캡처 변수를 가변 참조로 획득(&mut T)
+// FnOnce : 클로저의 캡처 변수를 값을 획득(T)
+fn apply<F>(f: F) where F: FnOnce() {
+    f();
+}
+// i32 를 반환하는 클로저를 파라메터로 받는 함수이며, 이 함수는 i32 를 반환한다.
+fn apply_to_3<F>(f: F) -> i32 where F:Fn(i32) -> i32 {
+    return f(3);
+}
+fn closure_as_input_parameters() {
+    let mut farewell = "goodbye".to_owned();
+    let greeting = "hello";
+
+    // 2개 변수를 캡처하게 된다. `greeting` 은 참조, `farewell` 은 값
+    let diary = || {
+        println!("I said {}.", greeting);
+
+        // 변경은 `farewell` 을 가변참조로 캡처되게 강제한다.
+        // 다음을 실행하려면 `FnMut` 선언이 필요함
+        farewell.push_str("!!!");
+        println!("Then I screamed {}.", farewell);
+        println!("Now I can sleep ....zzzz");
+
+        // 수동으로 drop 을 호출하면, `farewell` 을 값으로 캡처되도록 강제한다.
+        // 다음을 실행하려면 `FnOnce` 선언이 필요함.
+        std::mem::drop(farewell);
+    };
+
+    apply(diary);
+
+    let double = |x| { return 2 * x; };
+    println!("3 doubled : {}", apply_to_3(double));
+}
+
+fn capturing() {
+    let color = "green";
+    // `color` 를 출력하는 클로저는 `color` 를 즉각적으로 대여(&) 하고
+    // print 변수에 borrow와 클로저를 저장합니다.
+    // print 가 범위에서 벗어날때까지 borrow 는 유지됩니다.
+    let print = || println!("`color`:{}", color);
+    print();
+    print();
+
+    let mut count = 0;
+
+    // `count` 를 증가시키는 클로저는 `&mut count` 또는 `count` 중 하나를 취할수 있지만,
+    // `&mut count` 이 덜 제한적이므로, 이를 취합니다.
+    let mut inc = || { count +=1; println!("`count`: {}", count);};
+    inc();
+    inc();
+
+    let reborrow = &mut count;
+
+    let movable = Box::new(3);
+    // `mem::drop` 는 `T` 를 요구하므로, 이는 값을 취합니다.
+    // 복사 타입은 클로저로 복사되어 원본은 변경되지 않습니다.
+    // 복사가 아니라면, 이동되어야 합니다. 따라서, `movable` 이 클로저로 이동됩니다.
+    let consume = || { println!("`movable`: {:?}", movable); std::mem::drop(movable);};
+    consume();
 }
 fn closures() {
     fn function(i: i32) -> i32 {
@@ -17,7 +81,7 @@ fn closures() {
     let i = 1;
     println!("function: {}", function(i));
     println!("closure_annotated: {}", closure_annotated(i));
-    println!("closure_inferred: {}", closure_inferred(i));
+    println!("closure_inferred: {}", closure_inferred(i)); // note. infer 는 `추론하다` 라는 의미임.
 
     // 인자를 취하지 않는 closure 이며, 1을 반환한다.
     // 반환 타입은 추정됨.
