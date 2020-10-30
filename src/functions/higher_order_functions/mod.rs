14bd26dd0c8af356ec545cd8bda6d0d33e4294fd
Enji <seungrye@devguru.co.kr>
2020-10-30 10:08:41 +0900

    add example for higher order function

--- src/functions/higher_order_functions/mod.rs
+++ src/functions/higher_order_functions/mod.rs
@@ -1,3 +1,29 @@
+
 pub fn main() {
-    println!("WIP");
+    println!("Find the sum of all the squared odd numbers under 1000");
+    let upper = 1000;
+
+    // imperative approach
+    // declare accumulator variable
+    let mut acc = 0;
+    for n in 0.. {  // iterate: 0, 1, 2, ... to infinity
+        // square the number
+        let n_squared = n*n;
+        if n_squared >= upper {
+            break;
+        } else if is_odd(n_squared) {
+            acc += n_squared;
+        }
+    }
+    println!("imperative style : {}", acc);
+
+    let sum_of_squared_odd_numbers = (0..).map(|n| n*n) // all natural numbers squared
+        .take_while(|&n_suaqred| n_suaqred< upper) // below upper limit
+        .filter(|&n_squared| is_odd(n_squared)) // take odder
+        .fold(0, |acc, n_squared| acc + n_squared); // sum
+    println!("functional style: {}", sum_of_squared_odd_numbers);
+}
+
+fn is_odd(n: i32) -> bool {
+    return n % 2 == 1;
 }
